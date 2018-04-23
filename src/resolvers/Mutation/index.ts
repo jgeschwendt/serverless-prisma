6010c9d31e516447e433459a78ed32743f403c30
jlg <joshua@geschwendt.com>
2018-04-23 13:10:04 -0400

    switch to tslint

--- src/resolvers/Mutation/index.ts
+++ src/resolvers/Mutation/index.ts
@@ -1,10 +1,10 @@
 // https://medium.freecodecamp.org/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb
-function reverse(str) {
-  return (str === '') ? '' : reverse(str.substr(1)) + str.charAt(0);
+function reverse (str) {
+  return (str === '') ? '' : reverse(str.substr(1)) + str.charAt(0)
 }
 
 export default {
-  ping(_, { id }) {
+  ping (_, { id }) {
     return reverse(id)
   }
 }
